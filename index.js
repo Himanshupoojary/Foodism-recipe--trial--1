87456bd5ec73d1afa32363d66ec4fd3b69997807
HImanshu <110303027+Himanshupoojary@users.noreply.github.com>
2024-01-01 22:56:41 +0530

    updating

---
+++ index.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.nav-btn').addEventListener('click',function () {
+    document.querySelector('.nav-links').style.display='flex';  
+    document.querySelector('.nav-links').style.flexDirection='column';
+    document.querySelector('.nav-bar').style.height='20rem';
+})
+
