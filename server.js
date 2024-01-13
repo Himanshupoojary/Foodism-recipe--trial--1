545393e665bbded9f23461a7c6b4bc04043b03ee
HImanshu <110303027+Himanshupoojary@users.noreply.github.com>
2024-01-13 16:45:06 +0530

    optimisation

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
+import express from "express"
+import bodyParser from "body-parser";
+import { urlencoded } from "body-parser";
+
+
 const express= require("express");
 const app=express();
-const mongoose=require("mongoose");
+
 const bodyParser=require("body-parser");
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/",funtion(req,yes){
+app.get("/",(req,res) => {
+    res.send('sfsjfs')})
+    
+
+
+app.get("/about",(req,res) => {
     res.send("express is onn")})
+    
+app.get("/contact",(req,res) => {
+    res.send("express is onn")})
+    
 
-//app.post
+app.get("/addrecipe",(req,res) => {
+    res.send("express is onn")})
+    
 
+app.get("/recipe",(req,res) => {
+    res.send("express is onn")})
+                
+            
+                        
+    
 
 app.listen(3000,funtion(){
     console.log("server is running on 3000");
